89fc89173022d084e7468dc6ab4791bded918e7a
Dror Talmi <dror.gg.talmi@gmail.com>
2024-12-03 07:05:48 +0200

    what

---
+++ src/year2024/day3/Day3.java
@@ -0,0 +1,23 @@
+package year2024.day3;
+
+import java.net.URI;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class Day3 {
+
+    public static int solutionPart1(String input){
+        input.split("mul()")
+    }
+
+    public static void main(String[] args) {
+        try {
+            URI fileName = (new Object(){}).getClass().getResource("day.txt").toURI();
+            String input = Files.readAllLines(Paths.get(fileName)).toString();
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+}
